9938a133230df6f950ffcd026e21218f433d62f4
MaksPikul <128612499+MaksPikul@users.noreply.github.com>
2024-02-11 20:08:37 +0000

    Made notes on how the code works

--- TernaryStringTree/StringTree.py
+++ TernaryStringTree/StringTree.py
@@ -3,7 +3,236 @@ class StringTree:
         self.root = None
         self.size = 0
         self.dlist = DLinkedList()
+
+
+    
+    
+    def max(self):
+        if self.root == None: return None
+        return self._max(self.root).strFromTop()
+
+    # starts from the root,
+    # assigns pointer "ptr" as rightward node pointer,
+    # if no right pointer, it will keep going down middle to find the next rightward pointer or to check if multiplicity is bigger than 0
+    
+    def _max(self, node):
+        ptr = node
+        while True: 
+            while ptr.right == None: 
+                if ptr.mid == None:
+                    return ptr
+                ptr = ptr.mid
+            ptr = ptr.right
+
+
+
+    # will match each character of the given string "st" to traverse the tree to find string "st" until it reaches a node pointer towards "None"
+    # prevNode which saves the Node before "None" will have its multiplicity returned, which will be the count of the string
+
+    
+    def count(self, st):
+        ptr = self.root 
+        prevNode = ptr  # node which will hold the previous node when we reach ptr == None
+        i = 0           
         
+        while i != len(st):  # we do not move onto the next character unless we reach 
+            char = st[i]     # the same character in the Tree
+            temp = ptr
+            if ptr == None: # returns 0 if tree is empty before starting
+                return 0
+
+            if char == ptr.data:
+                ptr = ptr.mid
+                i = i + 1
+            elif char < ptr.data:
+                ptr = ptr.left
+            else:
+                ptr = ptr.right
+        return temp.mult
+
+    
+    # returns the largest string in the tree
+    # that is smaller than st. If no such string exists, return None
+
+    # traverses the tree to find the last node of string st, as it traverses down the tree, (if the ptr pointer has to traverse right or has to traverse down middle with a left pointer available)
+    # it saves the node in prev variable before continuing search of st through travsersing nodes
+    # it will continue to search for string "st".
+
+    # if ptr pointer traverses left, prev will be none, in that case, using the firstNode variable, it will find the biggest string on the left subtree of the firstNode variable which has been set when looking
+    # for the first char of string
+    
+    # Once "st" is found, the node saved in prev will return the string using method strFromTop() which shapes the string backwards as it goes up the tree
+    # or find the largest string in the sub-tree before using strFromTop().
+
+    def pred(self, st): 
+        if st == "" or self.root == None:
+            return None
+        firstNode = prev = ptr  = self.root.bin_search(st[0])
+        pred = firstNode.left
+        i = 0
+
+        while ptr and i < len(st):
+            prev = ptr
+            if st[i] < ptr.data:
+                ptr = ptr.left
+            elif st[i] > ptr.data:
+                pred = ptr
+                ptr = ptr.right
+            else:
+                if ptr.left:  
+                    pred = ptr.left
+                
+                i = i + 1
+                prev = ptr
+                ptr = ptr.mid
+        
+        
+        if pred == None:
+            if firstNode == self.root:
+                return None
+            if firstNode.parent.right == firstNode:
+                if firstNode.parent.mid:
+                    pred = firstNode.parent.mid
+                else:
+                    pred = firstNode.parent
+        
+        if pred:
+             
+            if pred.mult >= 1:
+                return pred.strFromTop()
+            else:
+                return self._max(pred).strFromTop()
+        else:
+            return None
+
+
+
+    # finds st and removes it, unless it doesnt fint it, does nothing instead
+    def remove(self, st):
+        #if tree is empty, does nothing and returns none
+        if st == "" or self.root == None:
+            return None
+
+        # finds node with the first character of string "st"
+        # begins traversal down the tree to find the last character of the string "st"
+
+        #during traversal, the last node will be in prev variable and ptr will be "None"
+        firstNode = ptr = self.root.bin_search(st[0])
+        i = 0
+        while i < len(st):
+            prev = ptr
+            
+            if ptr == None:
+                return None # word not found
+
+            if ptr.data == st[i]:
+                i = i + 1
+                ptr = ptr.mid
+            elif ptr.data > st[i]:
+                ptr = ptr.left
+            else:
+                ptr = ptr.right
+
+        # we check multiplicites of the last character
+        # mult of 0 means we haven't found string (not in tree) so we do nothing and return none
+        Node = None
+        if prev.mult == 0:
+            return None     
+
+        # multiplicity bigger than 1 means we can simply reduce the multiplicity by 1 without removing any nodes
+        elif prev.mult > 1:
+            prev.mult = prev.mult - 1
+            self.size = self.size - 1 
+            return None
+
+        # multiplicity is 1, so we will need to remove nodes
+
+        #first the multiplicity is reduced to 0, 
+        else:
+            Node = prev.dlistPtr
+            prev.mult = prev.mult - 1
+            ptr = prev
+
+            # when multiplicity is 0 and mid is none, we will remove the node until the guard proves false
+            while ptr.mult == 0 and ptr.mid == None:
+
+                #for when we reach root
+                if ptr == self.root:
+                    if ptr.right:
+                        self.root = ptr.right
+                        self.root.parent = None
+                        if ptr.left:
+                            if self.root.left:
+                                self.root.left.left = ptr.left
+                                ptr.left.parent = self.root.left
+                            else:
+                                self.root.left = ptr.left
+                                ptr.left.parent = self.root
+                    elif ptr.left:
+                        self.root = ptr.left
+                        self.root.parent = None
+                    else:
+                        self.root = None
+                        self.dlist = DLinkedList()
+                    break
+
+
+                # checks for right and left pointers, this is so that the right or left node becomes the new middle pointer can be assigned
+                # or to stop removing nodes if we hit a word which uses nodes from the string we removed
+                elif ptr.left or ptr.right:
+
+    
+                    if ptr.right:
+                        
+                        if ptr.parent.mult == 1 and ptr.mid:
+                            ptr.parent.right = ptr.right
+                            
+                        else:
+                            ptr.parent.mid = ptr.right
+                            ptr.right.left = ptr.left 
+                        ptr.right.parent = ptr.parent
+                        if ptr.left:
+                            ptr.left.parent = ptr.right
+
+                    
+                    else:
+                        if ptr.parent.mult == 1:
+                            ptr.parent.left = ptr.left
+                        else:
+                            ptr.parent.mid = ptr.left
+                        ptr.left.parent = ptr.parent
+                    break
+
+                # if node is not the root, and doesnt have right or left pointers, it will go up, comparing the node to be removed and its parents so that it can travel up the tree appropriately 
+                else:
+                    compare = ptr
+                    ptr = ptr.parent
+                    if compare == ptr.left:
+                        ptr.left = compare.left
+                        if compare.left:
+                            compare.left.parent = ptr
+                    elif compare == ptr.right:
+                        ptr.right = compare.right
+                        if compare.right:
+                            compare.right.parent = ptr
+                    else:
+                        ptr.mid = None
+
+
+        # removes 1 from tree size (number of strings in tree)
+        # removes node in doubly linked list (stores the full string, with the pointer to this string in the node of the last letter of each string in tree)
+        self.size = self.size - 1
+        self.dlist.remove(Node)
+        return None
+
+
+    
+
+
+    # These methods were provided
+
+
+    
     def __str__(self):
         if self.root == None: return "empty"
         return self.root.strTree()
@@ -64,49 +293,6 @@ class StringTree:
             while ptr.left != None: ptr = ptr.left
             if ptr.mult > 0: return ptr 
             ptr = ptr.mid
-            
-    # returns the largest string in the tree (None if tree empty)
-    def max(self):
-        if self.root == None: return None
-        return self._max(self.root).strFromTop()
-    
-    # returns the lexicographically maximal node in the tree rooted at node
-    
-    def _max(self, node):
-        ptr = node
-        while True: 
-            while ptr.right == None: 
-                if ptr.mid == None:
-                    return ptr
-                ptr = ptr.mid
-            ptr = ptr.right
-            
-
-
-    # returns the number of times that string st is stored in the tree
-    def count(self, st):
-        ptr = self.root # node which will be comparing data
-        prevNode = ptr  # node which will hold the previous node when we reach ptr == None
-        i = 0           
-        
-        while i != len(st):  # we do not move onto the next character unless we reach 
-            char = st[i]     # the same character in the Tree
-            temp = ptr
-            if ptr == None: # returns 0 if tree is empty before starting
-                return 0
-
-            if char == ptr.data:
-                ptr = ptr.mid
-                i = i + 1
-            elif char < ptr.data:
-                ptr = ptr.left
-            else:
-                ptr = ptr.right
-        return temp.mult
-   
-
-   
-
 
     # insert st in doubly linked list and return pointer to new list node
     # inefficient version
@@ -120,152 +306,8 @@ class StringTree:
             return self.dlist.append(st)
         if ptr.data == st: return None
         return self.dlist.insertLeft(st,ptr)
-        
-    # returns the smallest string in the tree
-    # that is larger than st. If no such string exists, return None
-    def succ(self, st): 
-        # TODO
-        return None
-        
-
-    # returns the largest string in the tree
-    # that is smaller than st. If no such string exists, return None
-    def pred(self, st): 
-        if st == "" or self.root == None:
-            return None
-        firstNode = prev = ptr  = self.root.bin_search(st[0])
-        pred = firstNode.left
-        i = 0
-
-        while ptr and i < len(st):
-            prev = ptr
-            if st[i] < ptr.data:
-                ptr = ptr.left
-            elif st[i] > ptr.data:
-                pred = ptr
-                ptr = ptr.right
-            else:
-                if ptr.left:  
-                    pred = ptr.left
-                
-
-                i = i + 1
-                prev = ptr
-                ptr = ptr.mid
-        
-        
-        if pred == None:
-            if firstNode == self.root:
-                return None
-            if firstNode.parent.right == firstNode:
-                if firstNode.parent.mid:
-                    pred = firstNode.parent.mid
-                else:
-                    pred = firstNode.parent
-        
-        if pred:
-             
-            if pred.mult >= 1:
-                return pred.strFromTop()
-            else:
-                return self._max(pred).strFromTop()
-        else:
-            return None
 
 
-    def remove(self, st):
-        if st == "" or self.root == None:
-            return None
-        
-        firstNode = ptr = self.root.bin_search(st[0])
-        i = 0
-        while i < len(st):
-            prev = ptr
-            
-
-            if ptr == None:
-                return None # word not found
-
-            if ptr.data == st[i]:
-                i = i + 1
-                ptr = ptr.mid
-            elif ptr.data > st[i]:
-                ptr = ptr.left
-            else:
-                ptr = ptr.right
-        
-        Node = None
-        #can check multiplicites and start removing
-        if prev.mult == 0:
-            return None      # st exists within a word in tree, check multiplicity to see if word in tree
-        elif prev.mult > 1:
-            prev.mult = prev.mult - 1
-            self.size = self.size - 1 
-            return None
-        else:
-            Node = prev.dlistPtr
-            prev.mult = prev.mult - 1
-            ptr = prev
-            while ptr.mult == 0 and ptr.mid == None:
-
-                if ptr == self.root:
-                    if ptr.right:
-                        self.root = ptr.right
-                        self.root.parent = None
-                        if ptr.left:
-                            if self.root.left:
-                                self.root.left.left = ptr.left
-                                ptr.left.parent = self.root.left
-                            else:
-                                self.root.left = ptr.left
-                                ptr.left.parent = self.root
-                    elif ptr.left:
-                        self.root = ptr.left
-                        self.root.parent = None
-                    else:
-                        self.root = None
-                        self.dlist = DLinkedList()
-                    break
-
-                elif ptr.left or ptr.right:
-                    
-                    if ptr.right:
-                        
-                        if ptr.parent.mult == 1 and ptr.mid:
-                            ptr.parent.right = ptr.right
-                            
-                        else:
-                            ptr.parent.mid = ptr.right
-                            ptr.right.left = ptr.left 
-                        ptr.right.parent = ptr.parent
-                        if ptr.left:
-                            ptr.left.parent = ptr.right
-                    else:
-                        if ptr.parent.mult == 1:
-                            ptr.parent.left = ptr.left
-                        else:
-                            ptr.parent.mid = ptr.left
-                        ptr.left.parent = ptr.parent
-                    break
-
-                else:
-                    compare = ptr
-                    ptr = ptr.parent
-                    if compare == ptr.left:
-                        ptr.left = compare.left
-                        if compare.left:
-                            compare.left.parent = ptr
-                    elif compare == ptr.right:
-                        ptr.right = compare.right
-                        if compare.right:
-                            compare.right.parent = ptr
-                    else:
-                        ptr.mid = None
-
-        
-        self.size = self.size - 1
-        self.dlist.remove(Node)
-        return None
         
         
 
